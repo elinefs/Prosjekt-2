68b77cc35bc12dd874d7946d5b6f6db5c4053ce9
elinefs <elinefs@stud.ntnu.no>
2017-03-02 12:50:53 +0100

    Oppgave 3

--- Prosjekt 2.py
+++ Prosjekt 2.py
@@ -1,3 +1,7 @@
+import random as random
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 alfa = 1.0610
 beta = 53.0516
@@ -5,10 +9,14 @@ a = alfa/12.
 b = -alfa/24.
 c = 0
 d = -alfa/beta
-N = 4
-import numpy as np
-import matplotlib.pyplot as plt
-#hudw
+N = 10
+
+fjærkonst = 0.1
+l = 5*10**(-9)
+r = 10**(-9)
+E = 300*10**6
+tmax = 1
+
 # Oppgave 1:
 A1 = [[1,0,0,beta/6,0,0,0,0,0,0,0,0,-1,0,0,0], # k = 0
      [0,1,0,0,0,0,0,0,0,0,0,0,-3,-1,0,0],
@@ -35,6 +43,12 @@ print(np.linalg.solve(A1,u1))
 
 # Oppgave 2a:
 
+def alpha(masse, l, E, r):
+    return ((9.81*masse*l**3)/(E*3.14*r**4/4))
+    
+def beta_k(fjærkonst, l, E, r):
+    return ((fjærkonst*l**3)/(E*3.14*r**4/4))
+
 def u_vektor(N):
     u = []
     for i in range(N):
@@ -45,13 +59,15 @@ def u_vektor(N):
     return u
 
     
-def A_matrise(N):
+def A_matrise(N, beta):
     s = 4*N
     A = np.zeros((s,s))
+    temp = 0
     for i in range(0,s,4):
         for j in range(i, i+4):
             A[j][j] = 1
-        A[i][i+3] = beta/6#(i/4)
+        A[i][i+3] = beta[temp]/6
+        temp += 1
         if i==0:
             for j in range(4):
                 A[i+j][s-(4-j)] = -1
@@ -71,13 +87,11 @@ def A_matrise(N):
     return A
 
 u = u_vektor(N)
-A = A_matrise(N)
+beta_liste = np.zeros(N)
+for i in range(N):
+    beta_liste[i] = beta #beta_k(fjærkonst, l, E, r)
+A = A_matrise(N, beta_liste)
 
-
-
-
-def beta(i):
-    return float(kappa(k)*(l**3)/B)
 
 
 ksi=np.linspace(0,N,1000*N)
@@ -103,19 +117,19 @@ def eta_trippelderivert(k,ksi):
     return (-(alfa*(ksi-k))+6*konstanter[4*k])
 
 for i in range (N):
-    for j in range (len(ksi)/N):
+    for j in range (int(len(ksi)/N)):
         eta_verdier[j+i*len(ksi)/N]=eta(i,ksi[j+i*len(ksi)/N])
 
 for i in range(N):
-    for j in range(len(ksi)/N):
+    for j in range(int(len(ksi)/N)):
         eta_derivert_verdier[j+i*len(ksi)/N] = eta_derivert(i,ksi[j+i*len(ksi)/N])
 
 for i in range(N):
-    for j in range(len(ksi)/N):
+    for j in range(int(len(ksi)/N)):
         eta_dobbeltderivert_verdier[j+i*len(ksi)/N] = eta_dobbeltderivert(i,ksi[j+i*len(ksi)/N])
 
 for i in range(N):
-    for j in range(len(ksi)/N):
+    for j in range(int(len(ksi)/N)):
         eta_trippelderivert_verdier[j+i*len(ksi)/N] = eta_trippelderivert(i,ksi[j+i*len(ksi)/N])
 
 
@@ -134,3 +148,52 @@ plt.show()
 plt.plot(ksi,eta_trippelderivert_verdier,'bo')
 plt.savefig("Oppgave 2a,trippelderivert.pdf")
 plt.show()
+
+
+
+
+
+def t_k(t_max, N):
+    tk = np.zeros(N)
+    for i in range(N):
+        tk[i] = random.random() * t_max
+        while tk[i] == 0:
+            tk[i] = random.random() * t_max
+    return tk
+    
+def r_k(beta, konstanter, tk, N):
+    d_j = np.zeros(N)
+    a = 0
+    for i in range(len(konstanter)):
+        if i%4 == 3:
+            d_j[a]=(konstanter[i])
+            a += 1
+    rk = np.zeros(N)
+    for i in range(N):
+        rk[i] = (-beta[i] * d_j[i])/tk[i]
+    return rk
+    
+    
+def oppgave3(t_max, N):
+    beta = np.zeros(N)
+    for i in range(N):
+        beta[i] = beta_k(fjærkonst, l, E, r)
+    tk = t_k(t_max, N)
+
+    
+    ryker = np.zeros(N)
+    alphaer = np.zeros(N)
+    for i in range(N):
+        A = A_matrise(N, beta)
+        u = u_vektor(N)
+        konstanter = np.linalg.solve(A,u)
+        rk = r_k(beta, konstanter, tk, N)
+        temp = np.argmax(rk)
+        alphaer[i] = beta[temp]/rk[temp]
+        beta[temp] = 0
+        ryker[i] = temp
+        
+    return ryker, alphaer 
+
+ryker, alphaer = oppgave3(tmax, N)
+print(ryker)
